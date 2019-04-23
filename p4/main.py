405369e082108299f9ba6b14b1d8736d55dca5bd
Sushant Ahuja <sushantahuja35@gmail.com>
2019-04-23 22:49:15 +0530

    Refactors the code to not use global variables

--- p4/main.py
+++ p4/main.py
@@ -5,7 +5,6 @@ Q3 = 'What does this expression evaluate to?' + "\n" 'True != (3 < 2)'
 
 user_tries = int(input("How many tries do you want for each question: "))
 
-right_answers = 0
 
 def askQuestion(question, answer, hint="Check the textbook"):
     no_of_tries = 0
@@ -14,22 +13,22 @@ def askQuestion(question, answer, hint="Check the textbook"):
         no_of_tries_left = user_tries - no_of_tries - 1
         no_of_tries += 1
 
-        global right_answers
-
         user_answer = str.lower(input("Your answer: "))
         if str.strip(user_answer) == str.lower(answer):
             print("Congratulations! You got it right.")
-            right_answers += 1
-            break
+            return 1
         else:
             if no_of_tries_left == 1:
                 print(hint)
             elif no_of_tries_left == 0:
                 print("You answered \'" + user_answer + "\'. The correct answer is \'" + str(answer) + "\'.")
+                print("You have this many remaining tries: " + str(no_of_tries_left))
+                return 0
             print("You have this many remaining tries: " + str(no_of_tries_left))
 
 
-askQuestion(Q1, 'c')
-askQuestion(Q2, 'str', "notice the quotes!")
-askQuestion(Q3, 'True', "Calcuate the right side first. Don't forget != means not equal to.")
+right_answers = 0
+right_answers += askQuestion(Q1, 'c')
+right_answers += askQuestion(Q2, 'str', "notice the quotes!")
+right_answers += askQuestion(Q3, 'True', "Calcuate the right side first. Don't forget != means not equal to.")
 print("You tried 3 questions and got " + str(right_answers) + " right.")
